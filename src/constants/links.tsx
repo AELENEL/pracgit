942cda38673763d17844db80d535b6f7a45e708d
Арлен Каримов <aelenel@MacBook-Air-Arlen.local>
2025-04-14 18:20:12 +0600

    check_crud

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -1,7 +1,14 @@
+import AdminPage from "../components/pages/AdminPage";
+
 export const links = [
   {
     link: "/",
     title: "home",
     el: "",
+  },
+  {
+    link: "/admin",
+    title: "admin",
+    el: <AdminPage />,
   },
 ];
