22cccb96867cd18e90b5efb48e36a14f39be81af
AELENEL <karimovarlen69@gmail.com>
2025-04-14 18:31:11 +0600

    Revert "check_crud"

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -1,14 +1,7 @@
-import AdminPage from "../components/pages/AdminPage";
-
 export const links = [
   {
     link: "/",
     title: "home",
     el: "",
-  },
-  {
-    link: "/admin",
-    title: "admin",
-    el: <AdminPage />,
   },
 ];
